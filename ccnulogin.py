b6151fadff8856aaeed55f06e40125a16ac2cb2e
SirlyDreamer <huanghongsen@outlook.com>
2021-11-21 21:44:55 +0800

    Fixed a problem : Connection reset when network is not stable

--- ccnulogin.py
+++ ccnulogin.py
@@ -6,43 +6,51 @@ headers = { "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 def login():
     username=""
     passwd=""
-    url = 'http://10.220.250.50/0.htm'
+    url = 'http://10.220.250.50/1.htm'
     payload = {"DDDDD":"%s"%username,
             "upass":"%s"%passwd,
             "suffix":"123456",
             "0MKKey":"123"}
-    try:
-        requests.post(url,data=payload,headers=headers)
-        return 1
-    except requests.exceptions.RequestException as errinfo:
-        print(errinfo)
-        return 0
+    for i in range(3):
+        try:
+            r = requests.post(url,data=payload,headers=headers)
+            if r.status_code == 200:
+                return True
+            time.sleep(1)
+        except requests.exceptions.RequestException as errinfo:
+            print(errinfo)
+    return False
 
 def testNet(url):
-    try:
-        r = requests.get(url,headers=headers,timeout=(3.05,12.05))
-        return r.status_code == 200
-    except requests.exceptions.RequestException as errinfo:
-        print(errinfo)
-        return 0
+    for i in range(3):
+        try:
+            r = requests.get(url,headers=headers,timeout=(3.05,12.05))
+            if r.status_code == 200:
+                return True
+            time.sleep(1)
+        except requests.exceptions.RequestException as errinfo:
+            print(errinfo)
+    return False
 
 def main():
     while True:
-        if(testNet("https://www.bilibili.com")):
+        if(testNet("https://www.baidu.com")):
             SleepTime = 600
             print (time.strftime("[%Y-%m-%d %H:%M:%S] Network Connect Succeed!", time.localtime()))  
         else:
             print (time.strftime("[%Y-%m-%d %H:%M:%S] Network Connect Failed, trying to login...", time.localtime()))
-            if(testNet("http://10.220.250.50")):
-                if(login()):
-                    SleepTime = 10
-                    print (time.strftime("[%Y-%m-%d %H:%M:%S] Login Request Sent", time.localtime()))
+            while(True):
+                if(testNet("http://10.220.250.50")):
+                    if(login()):
+                        SleepTime = 10
+                        print (time.strftime("[%Y-%m-%d %H:%M:%S] Login Request Sent", time.localtime()))
+                        break
+                    else:
+                        SleepTime = 30
+                        print (time.strftime("[%Y-%m-%d %H:%M:%S] Login Failed, will try 30s later...", time.localtime()))
                 else:
-                    SleepTime = 30
-                    print (time.strftime("[%Y-%m-%d %H:%M:%S] Login Failed, will try 30s later...", time.localtime()))
-            else:
-                SleepTime = 60
-                print (time.strftime("[%Y-%m-%d %H:%M:%S] Auth Server Connect Failed, waiting for 60s...", time.localtime()))
+                    SleepTime = 60
+                    print (time.strftime("[%Y-%m-%d %H:%M:%S] Auth Server Connect Failed, waiting for 60s...", time.localtime()))
         time.sleep(SleepTime)
 
 main()
