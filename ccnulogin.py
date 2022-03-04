094c265a6b8339d0217020c7d52f492b7de92e49
Sirly <45280500+SirlyDreamer@users.noreply.github.com>
2022-03-04 19:19:19 +0800

    Update User Agent

--- ccnulogin.py
+++ ccnulogin.py
@@ -1,7 +1,7 @@
 import requests
 import time
 
-headers = { "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36",
+headers = { "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36",
             "Content-Type": "application/x-www-form-urlencoded",}
             
 def login():
